4868635e1834b859da9e99c2c12341797c6826e0
duresamorina <50017876+duresamorina@users.noreply.github.com>
2019-05-31 15:54:01 +0200

    Add files via upload

---
+++ FurnizuesiInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+import java.util.List;
+
+/**
+ *
+ * @author lumib
+ */
+public class FurnizuesiInterface {
+    void create(Furnizuesi f)throws ClothesException;
+    void edit(Furnizuesi f)throws ClothesException;
+    void delete(Furnizuesi f)throws ClothesException;
+    List<Furnizuesi> findAll()throws ClothesException;
+    Furnizuesi findById(Integer id)throws ClothesException;
+}
